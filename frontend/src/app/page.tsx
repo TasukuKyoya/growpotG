9b6d3d7df359a300c6e46eba0e128088b50f2d4b
Tasuku Kyoya <t.kyoya@e-dash.io>
2025-06-10 14:45:22 +0900

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,9 +10,7 @@ export default function Home() {
         >
           投稿画面へ遷移
         </button>
-      </div>
-      <div>
- <button 
+        <button 
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
         >
           投稿画面へ遷移
